496c2edaf16ec3cc0228ecad6a83b19d4709b8e0
internetdrew <andrew@internetdrew.com>
2023-05-04 19:08:19 -0400

    add author component to index

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,4 @@ export { default as Contact } from './Contact';
 export { default as Footer } from './Footer';
 export { default as Layout } from './Layout';
 export { default as BlogCard } from './BlogCard';
+export { default as Author } from './Author';
