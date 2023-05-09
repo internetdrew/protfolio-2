dc535c9e6bb595115913f4cb17a603cbc1098ac1
internetdrew <andrew@internetdrew.com>
2023-05-09 16:54:00 -0400

    remove Head from layout and move to individual pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,11 @@
 import { useRecoilValue } from 'recoil';
 import { darkModeState } from '../atoms/darkModeAtom';
-import Head from 'next/head';
 import { Footer, Navbar } from '@/src/components';
 
-const Layout = ({ children, title }) => {
+const Layout = ({ children }) => {
   const darkMode = useRecoilValue(darkModeState);
   return (
     <div className={darkMode ? 'dark' : ''}>
-      <Head>
-        <title>{title}</title>
-        {/* Remember to add meta later on */}
-      </Head>
       <Navbar />
       <main className='bg-slate-100 min-h-screen text-gray-900 px-10 sm:px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
         {children}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,26 @@
+import Head from 'next/head';
 import { Hero, About, Projects, Contact, Layout } from '@/src/components';
 
 export default function Home() {
   return (
     <Layout title='Andrew Rowley — Software Engineer'>
+      <Head>
+        <title>Andrew Rowley — Software Engineer</title>
+        <meta property='og:title' content='Andrew Rowley — Software Engineer' />
+        <meta
+          property='og:image'
+          content='https://media.graphassets.com/lRDp723UQgq262BW8ryX'
+        />
+        <meta property='og:url' content='https://internetdrew.com' />
+        <meta name='twitter:card' content='summary_large_image' />
+
+        <meta
+          property='og:description'
+          content='The portfolio of Software Engineer, Andrew Rowley'
+        />
+        <meta property='og:site_name' content='Internet Drew' />
+        <meta name='twitter:image:alt' content='Alt text for image' />
+      </Head>
       <Hero />
       <About />
       <Projects />
