d80675f4ae6c0960587863dd9e5d6065ebb5c2c3
Andrew Rowley <andrew@internetdrew.com>
2023-07-31 14:44:36 -0400

    change description in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,16 +9,13 @@ const About = () => {
       <div className='grid md:grid-cols-2 gap-4 items-center md:items-start'>
         <div className='flex flex-col items-center gap-4 mb-10 md:items-start md:justify-start'>
           <h4 className='text-2xl font-bold text-center md:text-left md:text-3xl dark:text-slate-200'>
-            B2B Content Strategist turned Software Engineer
+            Frontend-focused Full-Stack Software Engineer
           </h4>
         </div>
         <p className='text-xl text-gray-800 dark:text-slate-200'>
           I'm Andrew, a passionate software engineer dedicated to crafting
           quality software with a focus on user experience, performance, and AI
           integration.
-          <br />
-          <br />
-          Let's build something extraordinary!
         </p>
       </div>
     </section>
