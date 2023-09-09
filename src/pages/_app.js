f590a13a8a6ad9e9177dde2d52a309d872612735
Andrew Rowley <andrew@internetdrew.com>
2023-09-09 14:20:10 -0400

    add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,22 @@
 import '@/styles/globals.css';
+import Script from 'next/script';
 import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }) {
   return (
     <RecoilRoot>
+      <Script
+        strategy='afterInteractive'
+        src='https://www.googletagmanager.com/gtag/js?id='
+      />
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3Q5Y9FR4KE')`}
+      </Script>
       <Component {...pageProps} />
     </RecoilRoot>
   );
